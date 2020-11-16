c3e2baca1122c83f0b9b414684d2cf589171f66f
John Cortes <john.cortes00@usc.edu.co>
2020-11-16 17:49:38 -0500

    Change db connection

--- src/index.js
+++ src/index.js
@@ -7,6 +7,6 @@ app.use(express.urlencoded({extended:false}));
 
 //routes
 app.use(require('./routes/index'));
-
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 app.listen(process.env.PORT||4000);
 console.log('Server on port 4000');
